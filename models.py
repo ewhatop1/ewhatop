d33140a33354c14a695a32c78d8e1a7f9040f95a
Bo0oeh <dmsdk4608@ewha.ac.kr>
2025-05-25 22:02:13 +0900

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)       # 고유 ID (자동 증가)
-    id_hash = db.Column(db.String(255), unique=True, nullable=False)   # 로그인 ID (유니크, 해싱)
+    login_id = db.Column(db.String(80), unique=True)   # 로그인 ID (유니크)
     
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
